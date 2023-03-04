2b4a77457248a72ee3eb4f285f7c2a537ef44795
duy-nhat-1 <duyisdenhat@gmail.com>
2023-03-04 09:48:52 +0700

    update swagger

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import cors from 'cors'
 const app = express()
 import initRoutes from './app/routes'
 import cookieParser from 'cookie-parser'
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger-output.json')
 app.use(cors({
     origin: process.env.CLIENT_URL,
     methods: ["POST", "GET", "PUT", "DELETE"]
@@ -12,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 import connectDB from './app/config/connectDB'
 app.use(express.urlencoded({ extended: true }))
-
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 const port = process.env.port || 8987
 
 const listener = app.listen(port, () => {
